7282aa26a175c6a3195f5ea3e4fd46c74dca3041
Yuri Ripardo <yuri_tbj.rj@hotmail.com>
2024-03-14 15:46:56 -0300

    Add files via upload

---
+++ dia-01/soma-dois-inteiros.py
@@ -0,0 +1,6 @@
+print("------SOMA DE DOIS NUMEROS INTEIROS------")
+
+num_1 = int(input("Digite o primeiro numero: "))
+num_2 = int(input("Digite o segundo numero: "))
+
+print(f"A soma de {num_1} + {num_2} é igual a: {num_1 + num_2}")
