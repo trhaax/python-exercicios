fe56b9c125afc6f595a34a1031dd6cf0ac7f2671
Yuri Ripardo <yuri_tbj.rj@hotmail.com>
2024-03-14 18:39:36 -0300

    Add files via upload

---
+++ dia-01/area-circulo.py
@@ -0,0 +1,7 @@
+print("-----------CALCULA A AREA DE UM CIRCULO-----------")
+
+raio = float(input("Digite o tamanho do raio do circulo: "))
+pi = float(input("Utilizar Pi igual a quanto? "))
+area = pi * (raio ** 2)
+
+print(f"A area do cirlculo é {area:.2f}")
---
+++ dia-01/celsius-para-fahrenheit.py
@@ -0,0 +1,6 @@
+print("------PROGRAMA PRA CONVERTER CELSIUS EM FAHRENHEIT------")
+
+celsius = float(input("Digite os graus em celsius: "))
+fahrenheit = 9 * celsius / 5 + 32
+
+print(f"{celsius}C° em Fahrenheit: {fahrenheit:.2f}F°")
